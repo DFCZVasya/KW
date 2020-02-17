35a95e9e77807b971ffc1649fe690bcfe15362d2
Vasya <lopatkin.2002@mail.com>
2020-02-17 21:17:30 +0700

    HW

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -5,17 +5,21 @@ using namespace std;
 
 void smth();
 
+
 typedef struct stack
 {
 	int* values;
 	int index, length;
 };
 
+int top(stack* p);
+int isEmpty(stack* p);
+
 stack* create_stack()
 {
 	stack* p = (stack*)malloc(sizeof(stack));
 	p->index = 0;
-	p->length = 2;
+	p->length = 1;
 	p->values = (int*)malloc(sizeof(int) * p->length);
 	return p;
 }
@@ -26,19 +30,64 @@ void destroy_stack(stack* p)
 	free(p);
 }
 
-
-void push(stack* p, int x);
-void pop(stack* p);
-void top(stack* p);
-void clear(stack* p);
-int isEmpty(stack* p)
+void show_stack(stack* p)
 {
-	return p->index == 0;
+	for (int i = 0; i < p->length; i++)
+	{
+		cout << p->values[i] << endl;
+	}
 }
+
+
+void push(stack* p, int x)
+{
+	if (p->index == p->length)
+	{
+		p->values = (int*)realloc(p->values, sizeof(int) * ++p->length);
+	}
+	p->values[p->index] = x;
+	p->index++;
+}
+
+int pop(stack* p) 
+{
+	if (!isEmpty(p))
+	{
+		int for_r = top(p);
+		p->index--;
+		p->values = (int*)realloc(p->values, sizeof(int) * --p->length);
+		return for_r;
+	}
+	else
+		return 0;
+}
+
+
+int top(stack* p)
+{
+	if (isEmpty(p))
+		return p->values[p->index];
+	else
+		return 0;
+}
+
+void clear(stack* p)
+{
+	destroy_stack(p);
+	create_stack();
+}
+int isEmpty(stack* p) {	return p->index == 0;}
 
 int main()
 {
-	
+	stack* a = create_stack();
+	push(a, 10);
+	push(a, 101);
+	push(a, 102);
+	show_stack(a);
+	pop(a);
+	show_stack(a);
+
 	system("pause");
 	return 0;
 }
