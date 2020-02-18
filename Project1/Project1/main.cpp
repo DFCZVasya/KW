8425085d7d08519af051356ef91a6d83d45307dd
Vasya Lopatkin <48513210+DFCZVasya@users.noreply.github.com>
2020-02-18 11:53:41 +0700

    Add files via upload

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -4,7 +4,171 @@
 using namespace std;
 
 void smth();
+typedef struct List {
+	int value;
+	struct List* next;
+} List;
 
+void printList(const List* head) {
+	while (head) {
+		printf("%d ", head->value);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+void push(List** head, int data) {
+	List* tmp = (List*)malloc(sizeof(List));
+	tmp->value = data;
+	tmp->next = (*head);
+	(*head) = tmp;
+}
+
+
+
+
+
+int pop(List** head) {
+	List* prev = NULL;
+	int val;
+	if (head == NULL) {
+		exit(-1);
+	}
+	prev = (*head);
+	val = prev->value;
+	(*head) = (*head)->next;
+	free(prev);
+	return val;
+}
+
+List* getNth(List* head, int n) {
+	int counter = 0;
+	while (counter < n && head) {
+		head = head->next;
+		counter++;
+	}
+	return head;
+}
+
+List* getLast(List* head) {
+	if (head == NULL) {
+		return NULL;
+	}
+	while (head->next) {
+		head = head->next;
+	}
+	return head;
+}
+
+void pushBack(List* head, int value)
+{
+	List* last = getLast(head);
+	List* tmp = (List*)malloc(sizeof(List));
+	tmp->value = value;
+	tmp->next = NULL;
+	last->next = tmp;
+}
+
+List* getLastButOne(List* head)
+{
+	if (head == NULL) {
+		exit(-2);
+	}
+	if (head->next == NULL) {
+		return NULL;
+	}
+	while (head->next->next) {
+		head = head->next;
+	}
+	return head;
+}
+
+void popBack(List** head) {
+	List* lastbn = NULL;
+	//Получили NULL
+	if (!head) {
+		exit(-1);
+	}
+	//Список пуст
+	if (!(*head)) {
+		exit(-1);
+	}
+	lastbn = getLastButOne(*head);
+	//Если в списке один элемент
+	if (lastbn == NULL) {
+		free(*head);
+		*head = NULL;
+	}
+	else {
+		free(lastbn->next);
+		lastbn->next = NULL;
+	}
+}
+
+
+void insert(List* head, unsigned n, int val) {
+	unsigned i = 0;
+	List* tmp = NULL;
+	//Находим нужный элемент. Если вышли за пределы списка, то выходим из цикла,
+	//ошибка выбрасываться не будет, произойдёт вставка в конец
+	while (i < n && head->next) {
+		head = head->next;
+		i++;
+	}
+	tmp = (List*)malloc(sizeof(List));
+	tmp->value = val;
+	//Если это не последний элемент, то next перекидываем на следующий узел
+	if (head->next) {
+		tmp->next = head->next;
+		//иначе на NULL
+	}
+	else {
+		tmp->next = NULL;
+	}
+	head->next = tmp;
+}
+
+
+int deleteNth(List** head, int n) {
+	if (n == 0) {
+		return pop(head);
+	}
+	else {
+		List* prev = getNth(*head, n - 1);
+		List* elm = prev->next;
+		int val = elm->value;
+
+		prev->next = elm->next;
+		free(elm);
+		return val;
+	}
+}
+
+void deleteList(List** head) {
+	List* prev = NULL;
+	while ((*head)->next) {
+		prev = (*head);
+		(*head) = (*head)->next;
+		free(prev);
+	}
+	free(*head);
+}
+
+
+
+int main()
+{
+	List* head;
+	pushBack(head, 11);
+	pushBack(head, 12);
+	pushBack(head, 13);
+	pushBack(head, 14);
+	printList(head);
+
+
+	system("pause");
+	return 0;
+}
 
 typedef struct stack
 {
@@ -77,20 +241,6 @@ void clear(stack* p)
 	create_stack();
 }
 int isEmpty(stack* p) {	return p->index == 0;}
-
-int main()
-{
-	stack* a = create_stack();
-	push(a, 10);
-	push(a, 101);
-	push(a, 102);
-	show_stack(a);
-	pop(a);
-	show_stack(a);
-
-	system("pause");
-	return 0;
-}
 
 
 void smth()
